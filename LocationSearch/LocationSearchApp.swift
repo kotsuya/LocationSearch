9c21268d5ead47420129ccc18605b8c59ab27508
kotsuya <kotsuya00@gmail.com>
2023-11-15 21:00:41 +0900

    Initial Commit

---
+++ LocationSearch/LocationSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSearchApp.swift
+//  LocationSearch
+//
+//  Created by Yoo on 2023/11/15.
+//
+
+import SwiftUI
+
+@main
+struct LocationSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
